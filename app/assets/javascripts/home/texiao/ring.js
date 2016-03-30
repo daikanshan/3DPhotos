8126b38720a26f5ff27877900535dde5ffe388ae
daikanshan <daikanshan@163.com>
2016-03-30 22:20:39 +0800

    add texioa

---
+++ app/assets/javascripts/home/texiao/ring.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require texiao/yuansu/three
+//= require 3Dshapes/cube
+$(document).ready(function(){
+  var scene,camera;
+  new Cube(5,5,5,0,0,0,table_img_medium[0],true);
+})
